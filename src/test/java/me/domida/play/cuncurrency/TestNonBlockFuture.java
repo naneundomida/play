eae85af3151937bfb36a14b6aace882b89aea90c
Sgmin <csm0594@gmail.com>
2021-04-13 01:00:08 +0900

    completable future stream execution

--- src/test/java/me/domida/play/cuncurrency/TestNonBlockFuture.java
+++ src/test/java/me/domida/play/cuncurrency/TestNonBlockFuture.java
@@ -7,8 +7,11 @@ import org.springframework.boot.test.autoconfigure.webservices.client.WebService
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class TestNonBlockFuture {
 
@@ -28,7 +31,7 @@ public class TestNonBlockFuture {
         //getPrice block이 있다. 500mlisecond
         List<String> shopPriceStr = shops.stream()
                 .map(shop -> String.format("%s price is %.2f", shop.getName(), shop.getPrice(shop.getName())))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         shopPriceStr.stream().forEach(System.out::println);
     }
@@ -38,9 +41,51 @@ public class TestNonBlockFuture {
         //getPrice block이 있다. 500mlisecond
         List<String> shopPriceStr = shops.parallelStream()
                 .map(shop -> String.format("%s price is %.2f", shop.getName(), shop.getPrice(shop.getName())))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         shopPriceStr.stream().forEach(System.out::println);
+    }
+
+    @Test
+    void futureFindPriceTest() {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        List<Future<String>> collect = shops.stream()
+                .map(shop -> executorService.submit(() -> {
+                    return String.format("%s price is %.2f", shop.getName(), shop.getPrice(shop.getName()));
+                }))
+                .collect(toList());
+
+        List<String> prices = collect.stream()
+                .map(future -> {
+                    try {
+                        return future.get();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                })
+                .collect(toList());
+
+        prices.forEach(System.out::println);
+    }
+
+
+    @Test
+    void completableFutureFindPriceTest() {
+        List<CompletableFuture<String>> priceFuture = shops.stream()
+                .map(shop -> CompletableFuture.supplyAsync(() ->
+                        String.format("%s price is %.2f", shop.getName(), shop.getPrice(shop.getName()))))
+                .collect(toList());
+
+        List<String> collect = priceFuture.stream()
+                .map(CompletableFuture::join) //get과 join의 차이는 checked exception 던지냐 안던지냐 차이
+                .collect(toList());
+
+        collect.forEach(System.out::println);
     }
 
 }
